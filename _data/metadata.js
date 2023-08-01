c5d678adae881b244712a139841b96c414b34ac5
Julius Figge <julius@figge.eu>
2023-08-01 07:46:54 +0200

    introduce icons, typo

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "JeuJeus Blog",
 	url: "https://blog.jeujeus.de",
 	language: "en",
-	description: "IT-Sec, Hacking, Coding, CS and stuff",
+	description: "IT-Sec, Hacking, Coding, CS and Stuff",
 	author: {
 		name: "Julius Figge",
 		email: "blog@jeujeus.de",
