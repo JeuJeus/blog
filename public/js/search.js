9998e467403346f2133dbacec38f1515d3ca9e9c
Julius Figge <julius.figge@klosebrothers.de>
2023-09-09 12:30:19 +0200

    refactor search

--- public/js/search.js
+++ public/js/search.js
@@ -1,26 +1,39 @@
+const includesSearchString = (postMeta, searchString) => postMeta.toLowerCase().includes(searchString);
+
+const postIncludesSearchString = (post, searchString) => includesSearchString(post.title, searchString) || includesSearchString(post.description, searchString) || includesSearchString(post.tags.toLowerCase(), searchString);
+
+const createPostResultEntry = post => {
+    const resultLink = document.createElement('a');
+    resultLink.href = post.url;
+    const resultLinkHeader = document.createElement('h2');
+    resultLinkHeader.innerText = post.title;
+    const resultLinkParagraph = document.createElement('p');
+    resultLinkParagraph.innerText = post.description;
+
+    resultLink.appendChild(resultLinkHeader);
+    resultLink.appendChild(resultLinkParagraph);
+
+    return resultLink;
+}
+
+function triggerSearch(posts, searchString, resultsDom) {
+    const results = posts
+        .filter(p => postIncludesSearchString(p, searchString))
+        .map((post) => createPostResultEntry(post));
+    resultsDom.replaceChildren(...results);
+}
+
+const handleKeyUpEvent = (event, posts, resultsDom) => {
+    const searchString = event.target.value.toLowerCase();
+    triggerSearch(posts, searchString, resultsDom);
+};
+
+const fetchPostsAsJson = async () => await fetch('/search.json').then(res => res.json());
+
 (async () => {
-    document.getElementById('search').addEventListener('keyup', (event) => {
-        const searchString = event.target.value.toLowerCase()
-        const results = []
-        posts.forEach((post) => {
-            if (
-                post.title.toLowerCase().includes(searchString) ||
-                post.description.toLowerCase().includes(searchString) ||
-                post.tags.toLowerCase().includes(searchString)
-            ) {
-                results.push(`<a href="${post.url}">
-          <h2>
-            ${post.title}
-          </h2>
-          <p>
-            ${post.description}
-          </p>
-        </a>`)
-            }
-        })
-
-        document.getElementById('results').innerHTML = results.join('')
-    })
-
-    const posts = await fetch('/search.json').then(res => res.json())
+    const resultsDom = document.querySelector('#results');
+    const searchDom = document.querySelector('#search');
+    const posts = await fetchPostsAsJson();
+    searchDom.addEventListener('keyup', keyUpEvent => handleKeyUpEvent(keyUpEvent, posts, resultsDom));
+    triggerSearch(posts, '', resultsDom);
 })()
